d6617a39a941501f3a7dfebca72d891f7e69b2f9
LDegraz <degrazial1@yahoo.com>
2025-03-23 13:05:37 -0400

    updated code based on errors

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { MONGODB_URI } from './config';
+import { MONGODB_URI } from './config.js';
 
 const connectDB = async () => {
   try {
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import routes from './routes/index';
+import routes from './routes/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
